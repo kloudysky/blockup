4d4d2bf66d11acea7161c7bdabb6a1c48d7a332b
thedaebu <min.kim163@gmail.com>
2021-03-30 22:44:42 -0400

    finish messages backend and messaged frontend without components

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,13 +1,10 @@
 import { combineReducers } from "redux";
 import session from "./session_api_reducer";
 import errors from "./errors_reducer";
-import MessagesReducer from "./messages_reducer";
 
 const RootReducer = combineReducers({
   errors,
-  messages: MessagesReducer,
   session,
 });
-
 
 export default RootReducer;
