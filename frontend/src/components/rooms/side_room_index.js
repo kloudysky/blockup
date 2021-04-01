124f6be32a8ddfa95984343109b152ab37ace5f9
Adam Kamboj <kamboj.adam@gmail.com>
2021-04-01 15:23:49 -0400

    :sparkle: ui

--- frontend/src/components/rooms/side_room_index.js
+++ frontend/src/components/rooms/side_room_index.js
@@ -9,8 +9,9 @@ import SideRoomItem from "./side_room_item";
 
 export class SideRoomIndex extends Component {
   componentDidMount() {
-    this.props.fetchUserRooms(this.props.user._id);
+    this.props.fetchUserRooms(this.props.user.id);
   }
+
   render() {
     return (
       <div className="sidebar">
@@ -30,9 +31,16 @@ export class SideRoomIndex extends Component {
         </div>
 
         <div className="sidebar-chats">
+          {this.props.rooms.legnth > 0 ? (
+            this.props.rooms.map((room) => (
+              <SideRoomItem key={room.id} id={room.id} name={room.name} />
+            ))
+          ) : (
+            <p>No rooms</p>
+          )}
+          {/* <SideRoomItem />
           <SideRoomItem />
-          <SideRoomItem />
-          <SideRoomItem />
+          <SideRoomItem /> */}
         </div>
       </div>
     );
--- frontend/src/components/rooms/side_room_item.js
+++ frontend/src/components/rooms/side_room_item.js
@@ -4,7 +4,7 @@ export class SideRoomItem extends React.Component {
   render() {
     return (
       <div className="sidebar-chat">
-        {/* <i class="fas fa-user-circle"></i> */}
+        <i class="fas fa-user-circle"></i>
         <div className="sidebar-chat-info">
           <h2>Room name</h2>
           <p>This is the last message in room</p>
