be0d0eb44c614109fc99130d64645a5b47679f6b
thedaebu <min.kim163@gmail.com>
2021-04-29 00:12:15 -0400

    create code that can help stop the 2FA message continuing to come up after the page is refreshed

--- frontend/src/components/web_app.js
+++ frontend/src/components/web_app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ChatBoxContainer from "./messages/chat_box_container";
 import SideRoomIndexContainer from "./rooms/side_room_index_container";
 
@@ -12,5 +13,40 @@ function WebApp() {
     </div>
   );
 }
+
+// class WebApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.twoFactorCheck = this.twoFactorCheck.bind(this);
+//   };
+  
+//   twoFactorCheck() {
+//     if (this.props.verified) {
+//       return (
+//         <div className="webapp">
+//           <div className="webappbody">
+//             <SideRoomIndexContainer />
+//             <ChatBoxContainer />
+//           </div>
+//         </div>
+//       );
+//     } else {
+//       return (
+//         <div>
+//           <Link to={"/twoFASetup"}>Please set up 2FA</Link>
+//         </div>
+//       )
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         {this.twoFactorCheck()}
+//       </div>
+//     )
+//   }
+// }
 
 export default WebApp;
